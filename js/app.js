852ee081b11cb2949f604ca2a78bc0973f115d92
Sergey Nelepa <nelepa1995@mail.ru>
2016-01-31 18:48:33 +0300

    add function for selected color button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-//alert("hello");
+
+var color = $(".activated").css("background-color");
+
+//click on buttons with color
+$(".controls li").click(function(){
+  console.log("click");
+  $(this).siblings().removeClass("activated");
+  $(this).addClass("activated");
+  //save current clicked color
+  color = $(this).css("background-color");
+});
