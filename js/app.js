db5d39df14a71e0344df73eb2736d642f2b97599
Sergey Nelepa <nelepa1995@mail.ru>
2016-01-31 20:18:52 +0300

    add function for painting

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 
 var color = $(".activated").css("background-color");
 
+var $canvas = $("canvas");
+var context = $canvas[0].getContext("2d");
+var lastEvent;
+var mouseDown = false;
 function changeColor(){
   var red = $("#red").val();
   var green = $("#green").val();
@@ -33,4 +37,23 @@ $("#addNewColor").click(function(){
   $newColor.css("background-color", $("#newColor").css("background-color"));
   $(".controls ul").append($newColor);
   $newColor.click();
+});
+
+$canvas.mousedown(function(e){
+  lastEvent = e;
+  mouseDown = true;
+}).mousemove(function(e){
+  if(mouseDown){
+    context.beginPath();
+    context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = color;
+    context.stroke();
+    lastEvent = e;
+  }
+
+}).mouseup(function(){
+  mouseDown = false;
+}).mouseleave(function(){
+  $canvas.mouseup();
 });
